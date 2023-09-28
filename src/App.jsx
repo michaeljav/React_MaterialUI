dce9da129d5f2b4384ec57aba47ffcf92d34221d
Michael Javier <michaeljaviermota@gmail.com>
2023-09-28 18:54:43 -0400

    LISTO PARA TRABAJAR  MATERIAL UI / BASE PROJECTO

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+// import { Button } from '@mui/material/Button';
+import { Button } from '@mui/material';
+
 export const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <h1>App</h1>
+      <Button variant='contained'>mi primer boton</Button>
+    </>
+  );
 };
