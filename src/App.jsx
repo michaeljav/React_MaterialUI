f5cb95d7ff58c537d59e39f5fa599d708952d6d4
Michael Javier <michaeljaviermota@gmail.com>
2023-09-28 19:16:43 -0400

    box

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 // import { Button } from '@mui/material/Button';
-import { Button, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Typography } from '@mui/material';
 
 export const App = () => {
   return (
     <Container sx={{ border: 2 }}>
       <h1>App</h1>
-      <Typography variant='h1' component='h2'>
-        hola
-      </Typography>
-      <Typography variant='h1'>hola h1</Typography>
-      <Typography variant='body1' textAlign='center' mt={5}>
-        hola h1
-      </Typography>
-      <Button variant='contained'>mi primer boton</Button>
+      <Box
+        sx={{
+          border: 2,
+          p: 5,
+          borderColor: 'peru',
+          bgcolor: '#111',
+          color: 'white',
+        }}
+        component='span'
+      >
+        es mui
+      </Box>
     </Container>
   );
 };
