0e54beb7fd3f83122214a5716007951e90611bb7
Michael Javier <michaeljaviermota@gmail.com>
2023-09-28 19:12:11 -0400

    container typografia explicada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 // import { Button } from '@mui/material/Button';
-import { Button } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 
 export const App = () => {
   return (
-    <>
+    <Container sx={{ border: 2 }}>
       <h1>App</h1>
+      <Typography variant='h1' component='h2'>
+        hola
+      </Typography>
+      <Typography variant='h1'>hola h1</Typography>
+      <Typography variant='body1' textAlign='center' mt={5}>
+        hola h1
+      </Typography>
       <Button variant='contained'>mi primer boton</Button>
-    </>
+    </Container>
   );
 };
