af5fc88e7205c4ffe6a499ad0ddefd4014a857b7
Michael Javier <michaeljaviermota@gmail.com>
2023-09-29 09:58:40 -0400

    notistack para alerta globales mejores

--- src/main.jsx
+++ src/main.jsx
@@ -23,13 +23,15 @@ const theme = createTheme({
 });
 
 import { BrowserRouter } from 'react-router-dom';
-
+import { SnackbarProvider } from 'notistack';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <CssBaseline />
-        <App />
+        <SnackbarProvider maxSnack={3} autoHideDuration={2000}>
+          <CssBaseline />
+          <App />
+        </SnackbarProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,23 @@
+import { Button } from '@mui/material';
+import { useSnackbar } from 'notistack';
 import React from 'react';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleClick = () => {
+    enqueueSnackbar('Suscribete con michael', {
+      variant: 'success',
+      anchorOrigin: { vertical: 'top', horizontal: 'right' },
+    });
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <Button variant='contained' onClick={handleClick}>
+        Open
+      </Button>
+    </>
+  );
 };
