7cea6f4209faea33ce356890edaf75c605d9da9e
Michael Javier <michaeljaviermota@gmail.com>
2023-09-29 18:19:02 -0400

    Inicio del projecto react typescript redux material ui

---
+++ ReactTypescriptReduxMui/ts-react-redux-material/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// rutas absolutas porque debo poner ...
+// import Home from './pages/Home';
+import Home from 'pages/Home';
+
+export default function App() {
+  return <Home />;
+}
