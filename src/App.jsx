71c27e1774c6e8a42aea45c3f147945078cc2591
Michael Javier <michaeljaviermota@gmail.com>
2023-09-28 21:02:23 -0400

    card y styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import { Container, Grid } from '@mui/material';
+import { Container } from '@mui/material';
+import { Product } from './components/Product';
 
 export const App = () => {
   return (
     <Container>
-      <Grid container spacing={5}>
-        <Grid item xs={12} sm={6} md={4}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, magnam
-            et. Esse, aperiam voluptate sed commodi ipsa perspiciatis
-            repellendus eaque ea deserunt delectus. Quibusdam odio illum quidem
-            minima quam! Iste?
-          </p>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, magnam
-            et. Esse, aperiam voluptate sed commodi ipsa perspiciatis
-            repellendus eaque ea deserunt delectus. Quibusdam odio illum quidem
-            minima quam! Iste?
-          </p>
-        </Grid>
-      </Grid>
+      <Product />
     </Container>
   );
 };
---
+++ src/components/Product.jsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { Box, Button, Paper, Typography } from '@mui/material';
+
+export const Product = () => {
+  const Img = styled('img')({
+    width: '200',
+    height: '100%',
+    objectFit: 'cover',
+    objectPosition: 'center',
+  });
+
+  return (
+    <Paper
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 2,
+        overflow: 'hidden',
+        mt: 5,
+      }}
+    >
+      <Img src='https://via.placeholder.com/200' alt='Mi imagen' />
+      <Box sx={{ flexGrow: 1, display: 'grid', gap: 2 }}>
+        <Typography variant='h4'>Product Name</Typography>
+        <Typography variant='body1'>Product Description</Typography>
+        <Button variant='contained'>Add Card</Button>
+      </Box>
+      <Box sx={{ mr: 2 }} component='p'>
+        $19.99
+      </Box>
+    </Paper>
+  );
+};
